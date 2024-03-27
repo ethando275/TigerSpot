8c48a0347fb1d639259759f4ad0f1cf94b34cb77
Winsice Ng <wn4759@princeton.edu>
2024-03-26 21:04:11 -0400

    metadata

--- cloud.py
+++ cloud.py
@@ -7,19 +7,24 @@ cloudinary.config(
   api_secret = 'wt-aWFLd0n-CelO5kN8h1NCYFzY'
 )
 
-# The name of the folder you want to list resources from
 folder_name = 'TigerSpot'
 
-# Fetch the resources
 resources = cloudinary.api.resources(
     type = 'upload',
-    prefix = folder_name,  # List resources under this folder
-    max_results = 500  # Adjust based on your needs, max is 500 per call
+    prefix = folder_name, 
+    max_results = 500,
+    context = True
 )
 
-# Extracting URLs and saving them to a file
-with open('url.txt', 'w') as f:
+with open('picturedata.txt', 'w') as f:
     for resource in resources.get('resources', []):
-        f.write(resource['url'] + '\n')  # Write each URL to the file
+        f.write(resource['url'] + '\n')
 
-print("URLs saved to url.txt")
+        custom_metadata = resource.get('context', {}).get('custom', {})
+        latitude = float(custom_metadata.get('Latitude'))
+        longitude = float(custom_metadata.get('Longitude'))
+        
+        f.write(f"{latitude}\n{longitude}\n")
+        #print(latitude + longitude)
+
+#print("TigerSpot's image data saved to picturedata.txt")
