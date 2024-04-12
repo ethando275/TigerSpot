27d96aceebfbf9280cb64c1a4f7804c022cd3f56
Winsice Ng <wn4759@princeton.edu>
2024-04-11 20:40:11 -0400

    rebase

--- database.py
+++ database.py
@@ -240,12 +240,16 @@ def insert_player(username, points): # can remove this points parameter
     conn.close()
 
 def calculate_today_points(distance):
-    if distance - 3 <= 0:
-        points = 100
-    elif distance - 10 <= 0:
-        points = 80
+    if distance - 15 <= 0:
+        points = 1000
     elif distance - 25 <= 0:
-        points = 50
+        points = 750
+    elif distance - 35 <= 0:
+        points = 500
+    elif distance - 45 <= 0:
+        points = 250
+    elif distance - 50 <= 0:
+        points = 100
     else:
         points = 0
     
@@ -1070,157 +1074,12 @@ def get_winner(challenge_id):
         if conn is not None:
             conn.close()
 
-def store_versus_pic_points(challenge_id, user_id, index, points):
-    conn = None
-    try:
-        conn = psycopg2.connect(DATABASE_URL)
-        cur = conn.cursor()
-        
-        # First, determine if the user is the challenger or the challengee for this challenge
-        cur.execute('''
-            SELECT challenger_id, challengee_id
-            FROM challenges
-            WHERE id = %s;
-        ''', (challenge_id,))
-        
-        result = cur.fetchone()
-        if result is None:
-            print("Challenge not found.")
-            return
-        
-        challenger_id, challengee_id = result
-        
-        # Depending on whether the user is the challenger or the challengee,
-        # update the corresponding points column in the challenges table
-        if user_id == challenger_id:
-            cur.execute('''
-                UPDATE challenges
-                SET challenger_points[%s] = %s
-                WHERE id = %s;
-            ''', (index, points, challenge_id))
-        elif user_id == challengee_id:
-            cur.execute('''
-                UPDATE challenges
-                SET challengee_pic_points[%s] = %s
-                WHERE id = %s;
-            ''', (index, points, challenge_id))
-        else:
-            print("User is not part of this challenge.")
-            return
-        
-        conn.commit()
-        print("Versus pic points updated successfully.")
-        
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(f"Error: {error}")
-    finally:
-        if conn is not None:
-            conn.close()
-
-def update_versus_pic_status(challenge_id, user_id, index):
-    conn = None
-    try:
-        conn = psycopg2.connect(DATABASE_URL)
-        cur = conn.cursor()
-        
-        # First, determine if the user is the challenger or the challengee for this challenge
-        cur.execute('''
-            SELECT challenger_id, challengee_id
-            FROM challenges
-            WHERE id = %s;
-        ''', (challenge_id,))
-        
-        result = cur.fetchone()
-        if result is None:
-            print("Challenge not found.")
-            return
-        
-        challenger_id, challengee_id = result
-        
-        # Depending on whether the user is the challenger or the challengee,
-        # update the corresponding boolean value in the challenges table
-        if user_id == challenger_id:
-            cur.execute('''
-                UPDATE challenges
-                SET challenger_bool[%s] = TRUE
-                WHERE id = %s;
-            ''', (index, challenge_id))
-        elif user_id == challengee_id:
-            cur.execute('''
-                UPDATE challenges
-                SET challengee_bool[%s] = TRUE
-                WHERE id = %s;
-            ''', (index, challenge_id))
-        else:
-            print("User is not part of this challenge.")
-            return
-        
-        conn.commit()
-        print("Versus pic status updated successfully.")
-        
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(f"Error: {error}")
-    finally:
-        if conn is not None:
-            conn.close()
-
-def get_versus_bool(challenge_id, user_id, index):
-    conn = None
-    try:
-        conn = psycopg2.connect(DATABASE_URL)
-        cur = conn.cursor()
-        
-        # First, determine if the user is the challenger or the challengee for this challenge
-        cur.execute('''
-            SELECT challenger_id, challengee_id
-            FROM challenges
-            WHERE id = %s;
-        ''', (challenge_id,))
-        
-        result = cur.fetchone()
-        if result is None:
-            print("Challenge not found.")
-            return None
-        
-        challenger_id, challengee_id = result
-        
-        # Depending on whether the user is the challenger or the challengee,
-        # get the corresponding boolean value from the challenges table
-        if user_id == challenger_id:
-            cur.execute('''
-                SELECT challenger_bool[%s]
-                FROM challenges
-                WHERE id = %s;
-            ''', (index, challenge_id))
-        elif user_id == challengee_id:
-            cur.execute('''
-                SELECT challengee_bool[%s]
-                FROM challenges
-                WHERE id = %s;
-            ''', (index, challenge_id))
-        else:
-            print("User is not part of this challenge.")
-            return None
-        
-        result = cur.fetchone()
-        if result is None:
-            print("No result found.")
-            return None
-        return result[0]
-        
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(f"Error: {error}")
-        return None
-    finally:
-        if conn is not None:
-            conn.close()
-
 def main():
-
     # update()
     # create_pic_table()
     # create_user_table()
-    # show_rows()
+    reset_players()
+    show_rows()
     # insert()
     # connection establishment
     # Creating a cursor object
