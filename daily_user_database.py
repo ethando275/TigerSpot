3447e1fda6f7e1991cb8788810db36878c913c7e
Winsice Ng <wn4759@princeton.edu>
2024-04-23 14:23:21 -0400

    fixed reset again...

--- daily_user_database.py
+++ daily_user_database.py
@@ -209,12 +209,21 @@ def remove_daily_user(username):
     conn.commit()
     conn.close()
 
+def remove_daily_user(username):
+    conn = psycopg2.connect(DATABASE_URL)
+    cur = conn.cursor()
+
+    cur.execute("DELETE FROM usersDaily WHERE username=%s;", (username,))
+
+    conn.commit()
+    conn.close()
+
 def main():
     # update_player_daily('wn4759', 100, 30)
     #reset_player('cl7359')
-    reset_player('wn4759')
+    #reset_player('wn4759')
     remove_daily_user('wn4759')
-    ##reset_player('jy1365')
+    #reset_player('jy1365')
     #create_daily_user_table()
     # date = get_last_played_date('fl9971')
     # print(date)
